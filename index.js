037714bb391bf9addbb8412734ecdadc326adb4f
Mrangulo-Ke <mrangulowanj@gmail.com>
2021-10-25 08:02:04 +0300

    adding routes and models

---
+++ index.js
@@ -0,0 +1,19 @@
+const  express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+    .connect(
+     process.env.MONGO_URL
+    )
+    .then(() => console.log("Database Connection was a success"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.listen(process.env.PORT, () => {
+    console.log("Backend server is running on port 3005");
+});
