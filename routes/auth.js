c1066dad0a6de6ce2d6e5c34ea128fe16161405a
Mrangulo-Ke <mrangulowanj@gmail.com>
2021-11-05 08:00:33 +0300

    minor updates

--- routes/auth.js
+++ routes/auth.js
@@ -39,9 +39,9 @@ router.post("/login", async (req, res) => {
             isAdmin: user.isAdmin,
         }, process.env.SEC_JWT, {expiresIn:"3d"});
 
-        const { password, ...others } = user._doc
+        const { password , ...others} = user._doc
 
-        res.status(200).json(others);
+        res.status(200).json({...others, accessToken});
     }catch(err){
         res.status(500).json(err);
     }
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,8 +1,10 @@
 const jwt = require("jsonwebtoken");
+//const { UserPage } = require("twilio/lib/rest/chat/v1/service/user");
 
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers.token;
     if (authHeader) {
+        const token = authHeader.split("")[1];
         jwt.verify(token, process.env.SEC_JWT, (err, user)=>{
             if(err) res.status(403).json("Token is not valid");
             req.user = user;
@@ -18,9 +20,23 @@ const verifyTokenAndAuthorization = (req,res,next) => {
         if (req.user.id === req.params.id || req.user.isAdmin) {
             next();
         } else {
-            res.status(403).json("You are not allowed to do that!");
+            res.status(403).json("NO priviledges to that!!");
         }
     });
 };
 
-module.exports = { verifyToken, verifyTokenAndAuthorization }
+const verifyTokenAndAdmin = (req,res, next) => {
+    verifyToken(req, res, () => {
+    if (req.user.isAdmin){
+        next();
+    } else {
+        res.status(403).json("NO priviledges to that!"):
+    }   
+    });
+};
+
+module.exports = { 
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin
+}
